5e7091c3598f3c7bb7c8da83b6aa014cd8f59611
Bianca Reymen <Bianca.Reymen@pandora.be>
2018-11-09 11:53:07 +0100

    oefening 18

--- src/main/java/be/vdab/frituurfrida/web/SnackController.java
+++ src/main/java/be/vdab/frituurfrida/web/SnackController.java
@@ -8,10 +8,13 @@ import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import be.vdab.frituurfrida.entities.Snack;
+import be.vdab.frituurfrida.exceptions.SnackNietGevondenException;
 import be.vdab.frituurfrida.services.SnackService;
 
 @Controller
@@ -19,6 +22,7 @@ import be.vdab.frituurfrida.services.SnackService;
 class SnackController {
 	private static final char[] ALFABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
 	private static final String ALFABET_VIEW = "alfabet";
+	private static final String WIJZIGEN_VIEW = "snackwijzigen";
 	private final SnackService snackService;
 
 	SnackController(SnackService snackService) {
@@ -60,4 +64,30 @@ class SnackController {
 		}
 		return modelAndView;
 	}
+
+	@GetMapping("{id}/wijzigen")
+	ModelAndView wijzigen(@PathVariable long id) {
+		System.out.print("test1");
+		ModelAndView modelAndView = new ModelAndView(WIJZIGEN_VIEW);
+		System.out.print("test2");
+		snackService.read(id).ifPresent(snack -> modelAndView.addObject(snack));
+		return modelAndView;
+	}
+
+	private static final String REDIRECT_URL_NA_WIJZIGEN = "redirect:/";
+	private static final String REDIRECT_URL_BIJ_SNACK_NIET_GEVONDEN = "snacknietgevonden";
+
+	@PostMapping("{id}/wijzigen")
+	String wijzigen(@Valid Snack snack, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return WIJZIGEN_VIEW;
+		}
+		try {
+			snackService.update(snack);
+			return REDIRECT_URL_NA_WIJZIGEN;
+		} catch (SnackNietGevondenException ex) {
+			return REDIRECT_URL_BIJ_SNACK_NIET_GEVONDEN;
+		}
+	}
+
 }
--- src/main/webapp/WEB-INF/JSP/beginnaam.jsp
+++ src/main/webapp/WEB-INF/JSP/beginnaam.jsp
@@ -23,8 +23,11 @@
 	</form:form>
 	<c:if test='${not empty snacks}'>
 		<ul>
-			<c:forEach items='${snacks}' var='snack'>
-				<li>${snack.naam}</li>
+			<c:forEach var='snack' items='${snacks}'>
+			<spring:url value='/snacks/{id}/wijzigen' var='url'>
+				<spring:param name='id' value='${snack.id}' />
+			</spring:url>
+				<li><a href ='${url}'>${snack.naam}</a></li>
 			</c:forEach>
 		</ul>
 	</c:if>
